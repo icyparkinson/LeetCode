5e6175696ad914ff8e204b71a690fdd4be8b3243
koulight <koulight@hotmail.com>
2022-06-30 16:06:38 -0700

    Another solution for checking anagram

--- 242_Valid_Anagram.js
+++ 242_Valid_Anagram.js
@@ -41,3 +41,37 @@
         
     } return true
 };
+
+function isAnagram(s1, s2){
+    if (s1.length !== s2.length) return false
+    let map1 = {}
+    let map2 = {}
+    for (let i = 0; i < s1.length; i++){
+        let c1 = s1[i]
+        let c2 = s2[i]
+        
+        if (map1[c1] === undefined){
+            map1[c1] = 1
+        } else{
+            map1[c1] += 1
+        }
+        
+        if (map2[c2] === undefined){
+            map2[c2] = 1
+        } else{
+            map2[c2] += 1
+        }
+    }
+    for (let char in map1){
+        if (map2[char] === undefined){
+            return false
+        }
+        if (map2[char] !== map1[char]){
+            return false
+        }
+    }
+    
+    return true
+}
+
+console.log(isAnagram("hat", "hat"))
