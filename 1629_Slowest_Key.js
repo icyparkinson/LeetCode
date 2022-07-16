77aa0a1517600ea1b0653a5b92e591428b766144
koulight <koulight@hotmail.com>
2022-07-16 09:27:52 -0700

    Shortened code with ternary

--- 1629_Slowest_Key.js
+++ 1629_Slowest_Key.js
@@ -4,20 +4,14 @@
  * @return {character}
  */
  var slowestKey = function(releaseTimes, keysPressed) {
-    let maxDiff = 0
     let slowest = keysPressed[0]
+    let maxDiff = 0
     for (let i = 0; i < keysPressed.length; i++){
-        let diff = 0
-        if (i === 0){
-            diff = releaseTimes[i]
-        }
-        if (i > 0){
-            diff = releaseTimes[i] - releaseTimes[i-1]
-        }
-        if (diff > maxDiff || diff === maxDiff && keysPressed[i] > slowest) {
+        let diff = i === 0 ? releaseTimes[0] : releaseTimes[i] - releaseTimes[i-1]
+        if (diff > maxDiff || diff === maxDiff && keysPressed[i] > slowest){
             maxDiff = diff
             slowest = keysPressed[i]
         }
-    } return slowest
+    }
+    return slowest
 };
-
