6340ee86f4b2dd84ca0fc501129140d3f302ab82
koulight <koulight@hotmail.com>
2022-07-19 21:01:26 -0700

    Dropbox, use trie, unsolved

---
+++ 0000_Dropbox_Keypad_Number.js
@@ -0,0 +1,23 @@
+/*
+Input = 
+["APPLE", "PLEAS", "PLEASE"]
+keypads = 
+["AELWXYZ", "AELPXYZ", "AELPSXY", "SAELPRT", "XAEBSKY"]
+
+Output = 
+[0, 1, 3, 2 0] where each number represents the number of words (from the wordlist) that can be formed using the keypad letters.
+
+First of all, the first letter of each keypad HAS to be in the word. (So the one starting with X, for example, would not work)
+*/
+
+function solve(wordlist, keypads){
+    for ()
+    
+    
+}
+
+
+
+let wl = ["APPLE", "PLEAS", "PLEASE"]
+let kp = ["AELWXYZ", "AELPXYZ", "AELPSXY", "SAELPRT", "XAEBSKY"]
+console.log(solve(wl, kp))
