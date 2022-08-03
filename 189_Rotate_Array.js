ac4ecdc13725df3fefdabefc95acfc05f2af0c1b
koulight <koulight@hotmail.com>
2022-08-02 22:52:00 -0700

    Create a helper function to reverse the array

--- 189_Rotate_Array.js
+++ 189_Rotate_Array.js
@@ -18,3 +18,27 @@
     reverse(k, nums.length-1, nums)
     
 };
+
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+ var rotate = function(nums, k) {
+    k%= nums.length
+    
+    function flip(start, end, nums){
+        while (start < end){
+            [nums[start], nums[end]] = [nums[end], nums[start]]
+            start++
+            end--
+        }
+        
+    }
+    
+    flip(0, nums.length-1, nums)
+    flip(0, k-1, nums)
+    flip(k, nums.length-1, nums)
+         
+};
+
