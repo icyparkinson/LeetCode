218b0993a4ee878e26d0cc229b040c576c7d3780
koulight <koulight@hotmail.com>
2022-08-06 10:57:36 -0700

    Did it again

--- 019_Remove_Nth_Node_From_End_of_List.js
+++ 019_Remove_Nth_Node_From_End_of_List.js
@@ -27,3 +27,33 @@
       left.next = left.next.next
       return dummy.next
   };
+
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @param {number} n
+ * @return {ListNode}
+ */
+ var removeNthFromEnd = function(head, n) {
+    let dummy = {val: -1, next: head}
+    let fast = head
+    let slow = dummy
+    while (n > 0){
+        fast = fast.next
+        n--
+    }
+    while (fast !== null){
+        fast = fast.next
+        slow = slow.next
+    }
+    slow.next = slow.next.next
+    return dummy.next
+    
+};
