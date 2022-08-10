309e29290fa9741a722714e239837c291a171039
koulight <koulight@hotmail.com>
2022-08-10 11:11:57 -0700

    Solved similarly another time

--- 121_Best_Time_to_Buy_and_Sell_Stock.js
+++ 121_Best_Time_to_Buy_and_Sell_Stock.js
@@ -24,3 +24,28 @@
     } return max
 };
 
+//another way:
+
+/**
+ * @param {number[]} prices
+ * @return {number}
+ */
+ var maxProfit = function(prices) {
+    let l = 0
+    let r = 1
+    let maxProf = 0
+    while (r < prices.length){
+        let prof = prices[r] - prices[l]
+        if (prof > maxProf){
+            maxProf = prof
+        }
+        if (prices[l] > prices[r]){
+            l++
+        }
+        if (prices[r] < prices[l]){
+            l = r
+        }
+        r++
+    }
+    return maxProf
+};
