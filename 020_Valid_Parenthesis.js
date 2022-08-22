524af834459a3dd194adfdfede7fcd2fd771c6d6
koulight <koulight@hotmail.com>
2022-08-22 14:53:30 -0700

    use object to keep track of closing bracket and its pair

--- 020_Valid_Parenthesis.js
+++ 020_Valid_Parenthesis.js
@@ -24,4 +24,34 @@
     
   }
 
+  //AGAIN:
+
+  /**
+ * @param {string} s
+ * @return {boolean}
+ */
+var isValid = function(s) {
+    let stack = []
+    let map = {
+        ")" : "(",
+        "]" : "[",
+        "}" : "{"
+    }
+    
+    for (let c of s){
+        if (map[c]){
+            //c is a closing bracket so we need to make sure that stack is not empty and that the last item in stack is a matching pair to c
+            if (stack[stack.length-1] === map[c]){
+                stack.pop()
+            } else{
+                return false
+            }
+        }else{
+            stack.push(c)
+        }
+    }
+    
+    return stack.length > 0 ? false : true
+}
+
   
