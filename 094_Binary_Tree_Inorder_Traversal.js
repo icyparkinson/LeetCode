5e745e280ed97de4d6b44777782bbac000951a5c
koulight <koulight@hotmail.com>
2022-08-24 15:18:07 -0700

    use recursive and put the answer.push where you want for preorder/inorder/postorder traversal

---
+++ 094_Binary_Tree_Inorder_Traversal.js
@@ -0,0 +1,23 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+ var inorderTraversal = function(root) {
+    let answer = []
+    if (!root) return answer
+    const traversal = (node) => {
+        if (node.left) traversal(node.left)
+        answer.push(node.val)
+        if (node.right) traversal(node.right)
+    }
+    traversal(root)
+    return answer
+};
