a7408c59c9517a68a573a2a7daa63c3f0250a3a6
koulight <koulight@hotmail.com>
2022-08-25 16:26:00 -0700

    Map everything and then put the keys in an array and iterate over them to find the one with value of 1

---
+++ 136_Single_Number.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+ var singleNumber = function(nums) {
+    let map = new Map()
+    for (let i = 0; i < nums.length; i++){
+        let v = nums[i]
+        if (!map.has(v)){
+            map.set(v, 1)
+        } else{
+            map.set(v, 2)
+        }
+    }
+    
+    let keys = Array.from(map.keys())
+    for (let key of keys){
+        if (map.get(key) === 1){
+            return key
+        }
+    }
+};
