c8a8bea8a130c71cd066b8d1659da3f97408230f
koulight <koulight@hotmail.com>
2022-08-25 11:43:52 -0700

    Compare substrings and return i if it is equal, -1 if not.

--- 028_Implement_strStr().js
+++ 028_Implement_strStr().js
@@ -6,3 +6,21 @@
  var strStr = function(haystack, needle) {
     return haystack.indexOf(needle)
 };
+
+/**
+ * @param {string} haystack
+ * @param {string} needle
+ * @return {number}
+ */
+ var strStr = function(haystack, needle) {
+    for (let i = 0; i < haystack.length; i++){
+        let c = haystack[i]
+        if (c === needle[0]){
+            let temp = haystack.substring(i, i+needle.length)
+            if (temp === needle){
+                return i
+            }
+        }
+    }
+    return -1
+};
