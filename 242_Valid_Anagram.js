40670522114dddb4de24d560337fb5851113e52d
koulight <koulight@hotmail.com>
2022-08-27 16:43:04 -0700

    Use just 1 map to get the freq of all the letters, then iterate over second string and subtract from map, returning false if it doesn't exist or freq is 0

--- 242_Valid_Anagram.js
+++ 242_Valid_Anagram.js
@@ -75,3 +75,32 @@ function isAnagram(s1, s2){
 }
 
 console.log(isAnagram("hat", "hat"))
+
+
+// Slightly more succinct and space-saving, removing one of the hash maps:
+
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+ var isAnagram = function(s, t) {
+    if (s.length !== t.length) return false
+    let map = new Map()
+    for (let i = 0; i < s.length; i++){
+        if (!map.has(s[i])){
+            map.set(s[i], 1)
+        }else{
+            map.set(s[i], map.get(s[i]) +1)
+        }
+    }
+    
+    for (let j = 0; j < t.length; j++){
+        if (map.get(t[j]) === 0 || !map.has(t[j])){
+            return false
+        }else{
+            map.set(t[j], map.get(t[j]) -1)
+        }
+    }
+    return true
+};
