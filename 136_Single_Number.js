4d25df74031cedd8d3f23776cd76626bd2e1fd70
koulight <koulight@hotmail.com>
2022-08-31 22:04:50 -0700

    Used a set instead, add on first sight, delete on second. Return what's left

--- 136_Single_Number.js
+++ 136_Single_Number.js
@@ -20,3 +20,19 @@
         }
     }
 };
+
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+ var singleNumber = function(nums) {
+    let set = new Set()
+    for (let i = 0; i < nums.length; i++){
+        if (!set.has(nums[i])){
+            set.add(nums[i])
+        }else{
+            set.delete(nums[i])
+        }
+    } return Array.from(set)[0]
+};
