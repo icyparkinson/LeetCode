08dc0f373c0cd2860e57d012eaacb8fe357cecac
koulight <koulight@hotmail.com>
2022-09-01 10:12:03 -0700

    Make adjacency list, then iterate through neighbors and recursively check if path exists

--- 1971_Find_if_Path_Exists_in_Graph.js
+++ 1971_Find_if_Path_Exists_in_Graph.js
@@ -45,3 +45,44 @@
     
     
 };
+
+
+/**
+ * @param {number} n
+ * @param {number[][]} edges
+ * @param {number} source
+ * @param {number} destination
+ * @return {boolean}
+ */
+ var validPath = function(n, edges, source, destination) {
+    const graph = buildGraph(edges)
+    let visited = new Set()
+    
+    return hasPath(graph, source, destination, visited)
+};
+
+const hasPath = (graph, src, dst, visited) => {
+    if (visited.has(src)) return false
+    visited.add(src)
+    if (src === dst) return true
+    for (let neighbor of graph[src]){
+        if (hasPath(graph, neighbor, dst, visited) === true){
+            return true
+        }
+    }
+    return false
+}
+
+
+const buildGraph = (edges) => {
+    let graph = {}
+    for (let edge of edges){
+        let [a, b] = edge
+        if (!graph[a]) graph[a] = []
+        if (!graph[b]) graph[b] = []
+        graph[a].push(b)
+        graph[b].push(a)
+    }
+    
+    return graph
+}
