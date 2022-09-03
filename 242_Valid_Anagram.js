661504cf29fb3aeea8d377fa30a924a795b8f7c8
koulight <koulight@hotmail.com>
2022-09-02 21:25:31 -0700

    Improved solution with more succinct 2nd half

--- 242_Valid_Anagram.js
+++ 242_Valid_Anagram.js
@@ -104,3 +104,30 @@ console.log(isAnagram("hat", "hat"))
     }
     return true
 };
+
+// Refactored again, even better for the 2nd half:
+
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+ var isAnagram = function(s, t) {
+    if (s.length !== t.length) return false
+    let sMap = new Map()
+    for (let i = 0; i < s.length; i++){
+        if (!sMap.has(s[i])){
+            sMap.set( s[i], 1)
+        }else{
+            sMap.set(s[i], sMap.get(s[i]) +1)
+        }
+    }
+    
+    for (let j = 0; j < t.length; j++){
+        if (sMap.get(t[j]) > 0){
+            sMap.set(t[j], sMap.get(t[j]) -1)
+        }else{
+            return false
+        }
+    }return true
+};
