4755c2aaaa210970b6b1cd8710e96b41c664df69
koulight <koulight@hotmail.com>
2022-09-05 17:54:27 -0700

    Create an empty array, then use dynamic programming

--- 056_Merge_Intervals.js
+++ 056_Merge_Intervals.js
@@ -30,3 +30,26 @@ const mergerInterval = (intervals, merged = []) => {
 
     return [ ...merged, prev ];
 };
+
+
+
+/**
+ * @param {number[][]} intervals
+ * @return {number[][]}
+ */
+ var merge = function(intervals) {
+    intervals.sort(([aStart, aEnd], [bStart, bEnd]) => aStart !== bStart ? aStart - bStart : aEnd - bEnd)
+    
+    let prev = intervals[0]
+    
+    let res = [prev]
+    for (let curr of intervals){
+        if(curr[0] <= prev[1]){
+            prev[1] = Math.max(curr[1], prev[1])
+        }else{
+            res.push(curr)
+            prev = curr
+        }
+    }
+    return res
+};
