8dbc0ede03cb65e9ae1262fb4b0d91e14ffecae9
koulight <koulight@hotmail.com>
2022-09-11 00:57:17 -0700

    flip the numbers between row and col and then reverse each row

--- 048_Rotate_Image.js
+++ 048_Rotate_Image.js
@@ -23,3 +23,19 @@
         right--
     }
 };
+
+//alternative method:
+
+var rotate = function(matrix) {
+    for (let row = 0; row < matrix.length; row++){
+        for (let col = row; col < matrix.length; col++){
+            let cur = matrix[row][col]
+            matrix[row][col] = matrix[col][row]
+            matrix[col][row] = cur
+        }
+    }
+    
+    for (let i = 0; i < matrix.length; i++){
+        matrix[i].reverse()
+    }
+};
