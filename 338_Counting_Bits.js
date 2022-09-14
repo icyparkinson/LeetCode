2f86abcd013490506dc831fdf673418298516815
koulight <koulight@hotmail.com>
2022-09-13 19:22:50 -0700

    Remember to use math.floor so you don't get NaN

--- 338_Counting_Bits.js
+++ 338_Counting_Bits.js
@@ -9,3 +9,17 @@
     }
     return output;
 };
+
+//another way:
+
+/**
+ * @param {number} n
+ * @return {number[]}
+ */
+ var countBits = function(n) {
+    let ans = [0]
+    for (let i = 1; i <= n; i++){
+        ans[i] = (ans[Math.floor(i/2)]) + (i%2)
+    }
+    return ans
+};
