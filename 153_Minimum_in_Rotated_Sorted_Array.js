f20c9662592ac039fb99b76b78d9c15f994e7b49
koulight <koulight@hotmail.com>
2022-09-18 12:35:03 -0700

    Use the sides to pinpoint the answer

--- 153_Minimum_in_Rotated_Sorted_Array.js
+++ 153_Minimum_in_Rotated_Sorted_Array.js
@@ -20,3 +20,27 @@
         }
     }
 };
+
+
+//more concise and easy to read:
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+ var findMin = function(nums) {
+    if (nums.length === 1) return nums[0]
+    let l = 0
+    let r = nums.length-1
+    while (l < r){
+        let m = l + Math.floor( (r-l) / 2)
+        if (nums[l] < nums[r]) return nums[l]
+        if (nums[m] > nums[m+1]) return nums[m+1]
+        if (nums[m] < nums[m-1]) return nums[m]
+        if (nums[m] > nums[l]){
+            l = m+1
+        }else{
+            r = m
+        }
+    }
+};
