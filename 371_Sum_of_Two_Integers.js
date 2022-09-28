9d65ec5bca39d5eb7cc548c407893ef7cc3a2ecb
koulight <koulight@hotmail.com>
2022-09-27 21:52:58 -0700

    Added comments to clarify bit manipulation

--- 371_Sum_of_Two_Integers.js
+++ 371_Sum_of_Two_Integers.js
@@ -5,9 +5,14 @@
  */
  var getSum = function(a, b) {
     while (b !== 0){
-        let temp = (a & b) <<1
+        //when you do a AND b, it returns a 1 only if both a and b is 1 but a 0 otherwise. Then move the value left by 1 digit to bring over the carry (which happens if there is a 1)
+        let tmp = (a & b) <<1
+        
+        //when you do a XOR b, it returns a 1 if either a or b is 1 but a 0 otherwise
         a = a ^ b
-        b = temp
+        
+        //use the tmp value, otherwise it would be altered by the previous step
+        b = tmp
     }
     return a
 };
